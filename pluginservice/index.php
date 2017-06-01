67302a379a0527736228147b6323de89cb20f41f
Elliot Smith <elliot.smith@bbc.co.uk>
2017-06-01 17:21:52 +0100

    Basic topic search working
    Topic detail display not in place yet.

--- pluginservice/index.php
+++ pluginservice/index.php
@@ -11,6 +11,123 @@ $app = new \Slim\App();
 
 $acropolisUrl = getenv('ACROPOLIS_URL');
 
+/* Flatten an array with non-numeric keys and array values; the key is
+   added as a property $keyAttribute to the value array */
+function flattenArray($arr, $keyAttribute)
+{
+    $arrOut = array();
+
+    foreach($arr as $key => $arrValue)
+    {
+        $arrValue[$keyAttribute] = $key;
+        $arrOut[] = $arrValue;
+    }
+
+    return $arrOut;
+}
+
+/* Merge 2 arrays with non-numeric keys; where the same key occurs in both arrays,
+   the data for the value of that key in the resulting array is itself an
+   array, containing as many non-empty values from the combined arrays as
+   possible */
+function mergeArrays($arr1, $arr2)
+{
+    $arrOut = array();
+
+    foreach($arr1 as $key => $data)
+    {
+        if(array_key_exists($key, $arr2))
+        {
+            if(is_array($arr1[$key]) && is_array($arr2[$key]))
+            {
+                $arrOut[$key] = mergeArrays($arr1[$key], $arr2[$key]);
+            }
+            else if(empty($arr2[$key]))
+            {
+                $arrOut[$key] = $arr1[$key];
+            }
+            else
+            {
+                $arrOut[$key] = $arr2[$key];
+            }
+        }
+        else
+        {
+            $arrOut[$key] = $data;
+        }
+    }
+
+    foreach($arr2 as $key => $data)
+    {
+        if(!array_key_exists($key, $arr1))
+        {
+            $arrOut[$key] = $data;
+        }
+    }
+
+    return $arrOut;
+}
+
+/* Extract media statements from a resource;
+   returns
+   array('players' => ..., 'contents' => ..., 'pages' => ...)
+ */
+function extractMedia($lodInstance)
+{
+    $result = array(
+        'players' => array(),
+        'contents' => array(),
+        'pages' => array()
+    );
+
+    $label = "{$lodInstance['dcterms:title,rdfs:label']}";
+    $description = "{$lodInstance['dcterms:description,rdfs:comment']}";
+
+    foreach($lodInstance['mrss:player'] as $player)
+    {
+        $result['players'] = mergeArrays($result['players'],
+            array(
+                $player->value => array(
+                    'source_uri' => $lodInstance->uri,
+                    'label' => $label,
+                    'description' => $description,
+                    'thumbnail' => "{$lodInstance['schema:thumbnailUrl']}",
+                    'height_px' => "{$lodInstance['exif:height']}",
+                    'width_px' => "{$lodInstance['exif:width']}",
+                    'date' => "{$lodInstance['dcterms:date']}",
+                    'location' => "{$lodInstance['lio:location']}"
+                )
+            )
+        );
+    }
+
+    foreach($lodInstance['mrss:content'] as $content)
+    {
+        $result['contents'] = mergeArrays($result['contents'],
+            array(
+                $content->value => array(
+                    'source_uri' => $lodInstance->uri,
+                    'label' => $label
+                )
+            )
+        );
+    }
+
+    foreach($lodInstance['foaf:page'] as $page)
+    {
+        $result['pages'] = mergeArrays($result['pages'],
+            array(
+                $page->value => array(
+                    'source_uri' => $lodInstance->uri,
+                    'label' => $label
+                )
+            )
+        );
+    }
+
+    return $result;
+}
+
 /*
  * paths:
  *
@@ -33,8 +150,8 @@ $app->get('/', function (Request $request, Response $response) {
 // call with /api/search?q=<search term>
 $app->get('/api/search', function(Request $request, Response $response) use($acropolisUrl) {
     $query = $request->getQueryParam('q', $default=NULL);
-    $limit = $request->getQueryParam('limit', $default=10);
-    $offset = $request->getQueryParam('', $default=0);
+    $limit = intval($request->getQueryParam('limit', $default=10));
+    $offset = intval($request->getQueryParam('offset', $default=0));
 
     $result = array(
         'acropolis_uri' => NULL,
@@ -63,23 +180,36 @@ $app->get('/api/search', function(Request $request, Response $response) use($acr
             $slotUri = $slot->value;
             $slotResource = $lod[$slotUri];
 
+            // if we can't resolve the slot resource, don't do anything
             if(!$slotResource)
             {
                 continue;
             }
 
+            // make proxy links to /api/topic/ for each resource
             foreach($slotResource['olo:item'] as $slotItem)
             {
                 if($slotItem->isResource())
                 {
                     $topic = $lod[$slotItem->value];
 
+                    $label = "{$topic['dcterms:title,rdfs:label']}";
+
+                    $isInfo = preg_match('|^Information about |', $label);
+
+                    // reject any foaf:Document resources whose label starts
+                    // with "Information about" - these are useless
+                    if($topic->hasType('foaf:Document') && $isInfo)
+                    {
+                        continue;
+                    }
+
                     $topicApiUri = $request->getUri()->withPath('/api/topic');
                     $topicApiUri = $topicApiUri->withQuery('uri=' . $topic->uri);
 
                     $result['items'][] = array(
                         'api_uri' => "$topicApiUri",
-                        'label' => "{$topic['dcterms:title,rdfs:label']}",
+                        'label' => $label,
                         'description' => "{$topic['dcterms:description,rdfs:comment']}"
                     );
                 }
@@ -87,11 +217,16 @@ $app->get('/api/search', function(Request $request, Response $response) use($acr
         }
     }
 
+    // do we have more results? (yes if xhtml:next statement present)
+    $result['hasNext'] = !empty("{$searchResultResource['xhtml:next']}");
+
     return $response->withJson($result);
 });
 
 // proxy for topic requests to Acropolis
-// call with /api/topic?uri=<acropolis URI>
+// call with /api/topic?uri=<acropolis URI>;
+// as we fetch RDF about the media, we merge it with anything we already
+// know about that piece of media
 $app->get('/api/topic', function(Request $request, Response $response) use($acropolisUrl) {
     $topicUri = $request->getQueryParam('uri', $default=NULL);
 
@@ -106,36 +241,65 @@ $app->get('/api/topic', function(Request $request, Response $response) use($acro
     $result = array(
         'uri' => "{$topic->uri}",
         'label' => "{$topic['rdfs:label,dcterms:title']}",
-        'description' => "{$topic['dcterms:description,rdfs:comment']}"
+        'description' => "{$topic['dcterms:description,rdfs:comment']}",
+        'players' => NULL,
+        'contents' => NULL,
+        'pages' => NULL
     );
 
-    // get olo:slots
-    $slots = $topic['olo:slot'];
+    $players = array();
+    $contents = array();
+    $pages = array();
 
-    // get olo:items for those olo:slots
-    $itemUris = array();
-    foreach($slots as $slot)
+    // find resources which are owl:sameAs the topic and extract media from
+    // them
+    $usefulUris = array($topic->uri) + $lod->getSameAs($topic->uri);
+
+    foreach($usefulUris as $usefulUri)
     {
-        $slotResource = $lod["$slot"];
-        $itemUris[] = '' . $slotResource['olo:item'];
+        $usefulResource = $lod[$usefulUri];
+        $media = extractMedia($usefulResource);
+        $players = mergeArrays($players, $media['players']);
+        $contents = mergeArrays($contents, $media['contents']);
+        $pages = mergeArrays($pages, $media['pages']);
     }
 
-    // fetch each item (NB these items are RES proxy resources without much
-    // detail in them)
-    foreach($itemUris as $itemUri)
+    // if we have olo:slots on the topic, fetch those and extract their media
+    // too
+    foreach($topic['olo:slot'] as $oloSlot)
     {
-        $lod->fetch($itemUri);
+        $slotResource = $lod[$oloSlot->value];
+        foreach($slotResource['olo:item'] as $slotItem)
+        {
+            $slotItemResource = $lod->fetch($slotItem->value);
+            $media = extractMedia($slotItemResource);
+            $players = mergeArrays($players, $media['players']);
+            $contents = mergeArrays($contents, $media['contents']);
+            $pages = mergeArrays($pages, $media['pages']);
 
-        // look for resources which are owl:sameAs the item URIs; this is
-        // because the original holds useful information which the RES proxy
-        // doesn't have, so we'd prefer to use the original if we can
+            // also get any resources which are sameAs the slot items, then
+            // find their foaf:primaryTopics and get media for them
+            $sameasUris = $lod->getSameAs($slotItem->value);
+            foreach($sameasUris as $sameasUri)
+            {
+                $sameasResource = $lod[$sameasUri];
 
-
-        // resolve those resources - we just use whatever RES has cached, so
-        // we don't need yet another fetch; if they can't be resolved, just use
-        // the proxy resource instead, which should have at least an mrss:player,
-        // mrss:content or foaf:page
+                foreach($sameasResource['foaf:primaryTopic'] as $primaryTopic)
+                {
+                    $primaryTopicResource = $lod->fetch($primaryTopic->value);
+                    $media = extractMedia($primaryTopicResource);
+                    $players = mergeArrays($players, $media['players']);
+                    $contents = mergeArrays($contents, $media['contents']);
+                    $pages = mergeArrays($pages, $media['pages']);
+                }
+            }
+        }
     }
+
+    // flatten out the arrays
+    $result['players'] = flattenArray($players, 'uri');
+    $result['contents'] = flattenArray($contents, 'uri');
+    $result['pages'] = flattenArray($pages, 'uri');
 
     return $response->withJson($result);
 });
--- pluginservice/js/app.js
+++ pluginservice/js/app.js
@@ -1,36 +1,223 @@
 // endpoint is the API endpoint on the pluginservice which proxies for RES
 // proper
 window.App = function (endpoint) {
-  this.searchForm = SearchForm('#search-form');
-  this.searchResultsPanel = SearchResultsPanel('#search-results-panel');
-  this.topicPanel = TopicPanel('#topic-panel');
-  this.client = RESClient(endpoint);
+  return {
+    init: function () {
+      var searchForm = SearchForm('#search-form');
+      var searchResultsPanel = SearchResultsPanel('#search-results-panel');
+      var topicPanel = TopicPanel('#topic-panel');
+      var client = RESClient(endpoint);
+      var eventCoordinator = EventCoordinator(
+        searchForm,
+        searchResultsPanel,
+        topicPanel,
+        client
+      );
+    }
+  };
 };
 
 var SearchForm = function (selector) {
+  var that = $({});
+
   var element = $(selector);
+  var input = element.find('[data-role=query-input]');
+  var button = element.find('[data-role=search-button]');
+
+  button.on('click', function (e) {
+    e.preventDefault();
+    that.trigger('search:send', input.val());
+  });
+
+  // disable the button and search input
+  that.disable = function () {
+    input.attr('disabled', 'disabled');
+    button.attr('disabled', 'disabled');
+  };
+
+  // enable button and input
+  that.enable = function () {
+    input.removeAttr('disabled');
+    button.removeAttr('disabled');
+  };
+
+  return that;
 };
 
 var SearchResultsPanel = function (selector) {
+  var that = $({});
+
   var element = $(selector);
+  var noSearchYetElement = element.find('[data-role=no-search-yet]');
+  var searchInProgressElement = element.find('[data-role=search-in-progress]');
+  var topicBoxContainerElement = element.find('[data-role=topic-box-container]');
+  var loadMoreButton = element.find('[data-role=load-more-button]');
+
+  loadMoreButton.on('click', function () {
+    that.trigger('results:more');
+  });
+
+  // clear search results and hide the search results heading
+  that.clear = function () {
+    topicBoxContainerElement.empty();
+    disableAllDivsExcept('');
+  };
+
+  // disable all the divs inside the container except the one
+  // whose data-role attribute matches dataRole
+  var disableAllDivsExcept = function (dataRole) {
+    element.children('div').each(function () {
+      elt = $(this);
+
+      if (elt.attr('data-role') === dataRole) {
+        elt.removeClass('ui-inactive');
+        elt.addClass('ui-active');
+      }
+      else {
+        elt.removeClass('ui-active');
+        elt.addClass('ui-inactive');
+      }
+    });
+  };
+
+  that.setNoSearchYet = function () {
+    disableAllDivsExcept('no-search-yet');
+  };
+
+  that.setSearchNoResults = function () {
+    disableAllDivsExcept('no-search-results');
+  };
+
+  that.setSearchResults = function () {
+    disableAllDivsExcept('search-results');
+  };
+
+  that.searchInProgress = function (bool) {
+    if (bool) {
+      // we've done a search, so we can hide the "please do a search" message
+      noSearchYetElement.removeClass('ui-active');
+      noSearchYetElement.addClass('ui-inactive');
+
+      // hide the load more button
+      loadMoreButton.removeClass('ui-active');
+      loadMoreButton.addClass('ui-inactive');
+
+      searchInProgressElement.removeClass('ui-inactive');
+      searchInProgressElement.addClass('ui-active');
+    }
+    else {
+      searchInProgressElement.removeClass('ui-active');
+      searchInProgressElement.addClass('ui-inactive');
+    }
+  };
+
+  that.setActive = function () {
+    element.removeClass('ui-inactive');
+    element.addClass('ui-active');
+  };
+
+  that.setInactive = function () {
+    element.removeClass('ui-active');
+    element.addClass('ui-inactive');
+  };
+
+  that.loadResult = function (result) {
+    var html = $(
+      '<div data-role="topic-box" class="panel panel-info">' +
+      '<div class="panel-body">' +
+      '<h2>' +
+      result.label +
+      '</h2>' +
+      '<p>' + result.description + '</p>' +
+      '</div>' +
+      '</div>'
+    );
+
+    // a click on the label link loads the topic
+    html.on('click', function (e) {
+      e.preventDefault();
+      that.trigger('results:load-topic', result.api_uri);
+    });
+
+    html.fadeIn();
+
+    topicBoxContainerElement.append(html);
+  };
+
+  that.hasResults = function () {
+    var topicBoxes = topicBoxContainerElement.find('[data-role=topic-box]');
+    return topicBoxes.length > 0;
+  };
+
+  that.loadResults = function (results) {
+    // add the results (if any) to the container
+    for (var i = 0; i < results.items.length; i++) {
+      that.loadResult(results.items[i]);
+    }
+
+    // if we've got more results available, show the "Load more" button
+    if (results.hasNext) {
+      loadMoreButton.removeClass('ui-inactive');
+      loadMoreButton.addClass('ui-active');
+    }
+    else {
+      loadMoreButton.removeClass('ui-active');
+      loadMoreButton.addClass('ui-inactive');
+    }
+
+    // count the number of results in the container and turn on "no results"
+    // area if appropriate; otherwise show the results area
+    if (that.hasResults()) {
+      that.setSearchResults();
+    }
+    else {
+      that.setSearchNoResults();
+    }
+  };
+
+  return that;
 };
 
 var TopicPanel = function (selector) {
+  var that = $({});
+
   var element = $(selector);
+
+  that.setActive = function () {
+    element.removeClass('ui-inactive');
+    element.addClass('ui-active');
+  };
+
+  that.setInactive = function () {
+    element.removeClass('ui-active');
+    element.addClass('ui-inactive');
+  };
+
+  return that;
 };
 
 var RESClient = function (endpoint) {
-  var offset = 0;
+  var that = $({});
 
-  this.reset = function () {
+  var offset = 0;
+  var limit = 10;
+  var lastQuery = null;
+
+  that.reset = function () {
     offset = 0;
   };
 
-  this.search = function (query) {
+  that.setOffset = function (newOffset) {
+    offset = newOffset;
+  };
+
+  that.search = function (query) {
+    lastQuery = query;
+
     var url = endpoint + '?q=' + encodeURIComponent(query) +
       '&offset=' + offset;
 
-    // TODO trigger fetching URL event
+    console.log('search URL: ' + url);
 
     $.ajax({
       url: url,
@@ -38,16 +225,104 @@ var RESClient = function (endpoint) {
       dataType: 'json',
 
       success: function (content) {
-        // TODO trigger results event
+        that.trigger('client:results', content);
       },
 
       error: function (err) {
-        // TODO trigger error event
+        that.trigger('client:error', err);
       }
     });
   };
 
-  // topicUri is the full URI for the topic
-  this.topic = function (topicUri) {
+  that.more = function () {
+    if (!lastQuery) {
+      return;
+    }
+
+    offset += limit;
+    that.search(lastQuery);
   };
+
+  return that;
+};
+
+var EventCoordinator = function (searchForm, searchResultsPanel, topicPanel, client) {
+  // handler for clicks on the search button
+  searchForm.on('search:send', function (e, query) {
+    // return if there's no search term
+    if (query === '') {
+      return;
+    }
+
+    // disable the search form until search completes
+    searchForm.disable();
+
+    // hide the topic panel, show the search panel
+    topicPanel.setInactive();
+    searchResultsPanel.setActive();
+
+    // clear the search panel and mark as "waiting"
+    searchResultsPanel.clear();
+    searchResultsPanel.searchInProgress(true);
+
+    // reset the client's paging
+    client.reset();
+
+    // perform the search
+    client.search(query);
+  });
+
+  // handler for clicks on the "load more" button
+  searchResultsPanel.on('results:more', function (e) {
+    // disable the search form until search completes
+    searchForm.disable();
+
+    // hide the topic panel, show the search panel
+    topicPanel.setInactive();
+    searchResultsPanel.setActive();
+
+    // mark search in progress but don't clear panel
+    searchResultsPanel.searchInProgress(true);
+
+    // perform the search
+    client.more();
+  });
+
+  // handler for clicks on topics
+  searchResultsPanel.on('results:load-topic', function (e, topicUri) {
+    // hide the search panel, show the topic panel
+    topicPanel.setActive();
+    searchResultsPanel.setInactive();
+
+    // show topic loading message
+
+  });
+
+  // handler for returned topic
+  client.on('client:topic', function (e, content) {
+    // hide the search panel, show the topic panel
+    topicPanel.setActive();
+    searchResultsPanel.setInactive();
+
+    // hide topic loading message
+
+
+    // load topic data into topic panel
+  });
+
+  // handler for returned search results
+  client.on('client:results', function (e, content) {
+    // re-enable the search form
+    searchForm.enable();
+
+    // hide the topic panel, show the search panel
+    topicPanel.setInactive();
+    searchResultsPanel.setActive();
+
+    // turn off search in progress
+    searchResultsPanel.searchInProgress(false);
+
+    // load results into the search results panel
+    searchResultsPanel.loadResults(content);
+  });
 };

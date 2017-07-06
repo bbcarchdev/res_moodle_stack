cda196abcc23e3249cadb2e423c6ceda6c9ad10a
Elliot Smith <elliot.smith@bbc.co.uk>
2017-07-06 11:09:04 +0100

    Update plugin service distro
    Needs edits to PLUGINSERVICE_URL: this variable
    is now redundant as the plugin service is served direct from
    the Moodle plugin subdirectory.

--- aws-task-def.sh
+++ aws-task-def.sh
@@ -4,10 +4,8 @@
 # START: EDIT THESE VARIABLES
 export MOODLE_PLUGIN_IMAGE=075239016712.dkr.ecr.eu-west-1.amazonaws.com/res-moodle-plugin_moodle
 export MARIADB_IMAGE=075239016712.dkr.ecr.eu-west-1.amazonaws.com/res-moodle-plugin_mariadb
-export PLUGINSERVICE_IMAGE=075239016712.dkr.ecr.eu-west-1.amazonaws.com/res-moodle-plugin_pluginservice
 
 export MOODLE_URL=http://ec2-34-250-205-153.eu-west-1.compute.amazonaws.com
-export PLUGINSERVICE_URL=http://ec2-34-250-205-153.eu-west-1.compute.amazonaws.com:8888/minimal.html
 
 export MYSQL_DATABASE=moodle
 export MYSQL_USER=moodle
